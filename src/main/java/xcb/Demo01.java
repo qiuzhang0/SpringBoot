4640473b93a5d9ce2e70b85008e397ada59614d8
qiuzhang <3057553239@qq.com>
2022-07-18 16:29:05 +0800

    使用idea集成git 实现提交信息 第一次修改~~~

--- src/main/java/xcb/Demo01.java
+++ src/main/java/xcb/Demo01.java
@@ -3,5 +3,6 @@ package xcb;
 public class Demo01 {
     public static void main(String[] agr) {
             System.out.println("first  test!");
+            System.out.println("second test!");
     }
 }
